086dc4759128e367eea4b1a8ec7e1570a75551c6
gillic <gillic@wix.com>
2026-04-01 14:13:46 +0300

    first commit

---
+++ server/src/test/java/com/plant_pals/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plant_pals.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
